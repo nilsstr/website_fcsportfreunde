9376f4220578d236063d7a5bc01c9727f9e3eea3
nilsstr <82819482+nilsstr@users.noreply.github.com>
2025-02-28 23:35:25 +0100

    Delete src directory

--- src/components/footer/footer.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import { Footer, FooterCopyright} from 'flowbite-svelte'
-    import { base } from '$app/paths';
-  </script>
-  
-  <Footer footerType="logo">
-    <FooterCopyright href="{base}/" by="FC Sportfreunde München" />
-  </Footer>
